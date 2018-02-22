7cc0bca720924f54cc91f5a7025e13ed2dcf1a68
Judy Najin Chun <judynajinchun@Judys-MacBook-Air-3.local>
2018-02-22 10:57:33 -0800

    message

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,4 +10,11 @@ $(document).ready(function() {
  */
 function initializePage() {
 	// your code here
+	$(".likeBtn").click(clickLikeBtn);
+}
+
+function clickLikeBtn(e) {
+	e.preventDefault();
+	ga('create', 'UA-cxxx', 'auto');
+	ga("send", "event",<Category>, <Action>);
 }
